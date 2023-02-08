e7d7f32e29ba74e8edfdfa5e3dd27978d495b232
billu <billu@LAPTOP-59BKH7IN>
2023-02-08 19:53:54 +0530

    first commit

---
+++ puja/src/puja/D0while.java
@@ -0,0 +1,15 @@
+package puja;
+
+public class D0while {
+
+	public static void main(String[] args) {
+		int i=1;
+		do {
+			System.out.println(i);
+			i++;
+		}
+		while(i<=5);
+
+	}
+
+}
---
+++ puja/src/puja/Loops.java
@@ -0,0 +1,13 @@
+package puja;
+
+public class Loops {
+
+	public static void main(String[] args) {
+		for(int i=0;i<3;i++)
+			System.out.println(i);
+	}
+			
+
+	}
+
+
